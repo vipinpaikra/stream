40839e5a13d37d4d565d61d46c781868979e787e
Vipin Paikra <vipinpaikra01@gmail.com>
2024-05-13 14:13:58 +0530

    video player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import VideoPlayer from "./VideoPlayer";
+import { useRef } from "react";
+import videojs from "video.js";
 
 function App() {
+  const playerRef = useRef(null);
+  const videoLink =
+    "http://localhost:8000/uploads/courses/853ec075-4764-4605-a56c-7aec19bae5ad/index.m3u8";
+
+  const videoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will disose");
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>vidoe player</h1>
+      <VideoPlayer options={videoPlayerOptions} onReady={handlePlayerReady} />
     </div>
   );
 }
